c0f578564ee7707b92e07e0c75d0c3ecb6c9d106
RafidRisun <rafidhassanrisun@gmail.com>
2026-07-18 21:28:32 +0600

    profile modal

--- app/about_us/page.tsx
+++ app/about_us/page.tsx
@@ -3,13 +3,18 @@ import Divider from "@/components/Divider";
 import { teamMembers } from "@/constants/data";
 import { setHamburgerClicked } from "@/redux/hamburgerClickedSlice";
 import Image from "next/image";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { IoMdClose } from "react-icons/io";
 
 export default function AboutUs() {
   const dispatch = useDispatch();
   function clickedAnywhere() {
     dispatch(setHamburgerClicked(false));
   }
+
+  const [personModalOpen, setPersonModalOpen] = useState(true);
+
   return (
     <div
       className="flex flex-col py-32 w-full bg-white gap-15 text-[#104649] text-justify"
@@ -74,10 +79,10 @@ export default function AboutUs() {
       <div className="flex flex-col items-center gap-15 p-5">
         <div className="flex flex-wrap items-center justify-center text-[#104649] font-light w-full">
           {teamMembers.map((member) => (
-            <a
+            <button
               key={member.id}
-              href={`/team/${member.id}`}
-              className="m-4 flex flex-col justify-between hover:scale-105 transition-transform duration-300 ease-in-out"
+              onClick={() => setPersonModalOpen(true)}
+              className="m-4 flex flex-col justify-between hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer"
             >
               <div className="relative w-75 h-75">
                 <Image
@@ -88,7 +93,7 @@ export default function AboutUs() {
                 />
               </div>
               <h3 className="text-lg font-bold mt-2">{member.name}</h3>
-            </a>
+            </button>
           ))}
         </div>
       </div>
@@ -141,6 +146,46 @@ export default function AboutUs() {
           </div>
         </div>
       </div>
+      {personModalOpen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-white/40 flex items-center justify-center z-50"
+          onClick={() => setPersonModalOpen(false)}
+        >
+          <div
+            className="w-3/5 h-4/5 flex bg-white relative"
+            onClick={(event) => event.stopPropagation()}
+          >
+            <button
+              className="absolute top-5 right-5 text-2xl hover:scale-130 transition-transform duration-300 ease-in-out cursor-pointer"
+              onClick={() => setPersonModalOpen(false)}
+            >
+              <IoMdClose />
+            </button>
+            <div className="w-1/3 h-full relative">
+              <Image
+                src="/images/aboutus/cat.jpg"
+                alt="project 1"
+                fill
+                className={`object-cover`}
+              />
+            </div>
+            <div className="w-2/3 h-full flex flex-col justify-between p-15">
+              <div className="flex flex-col gap-10">
+                <h3 className="text-5xl font-thin">Chengis Khan</h3>
+                <p>
+                  Chengis Khan is a passionate musician with over 10 years of
+                  experience in the industry. He has performed in various venues
+                  and is known for his innovative approach to music.
+                </p>
+              </div>
+              <div className="flex flex-col gap-5">
+                <h4 className="text-2xl font-normal">Founder</h4>
+                <h4 className="text-xl font-light">BUET</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
